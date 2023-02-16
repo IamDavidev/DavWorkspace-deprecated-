ad09de0284c2f0a681765ff801e5715c94b8a5ac
itsDavidev <davidlez2mgk@gmail.com>
2023-02-16 14:53:51 -0600

    [ configuration 🔧 ] : colors tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#0f9'
+        secondary: "#1b1b1b",
+        primary: "#db9ee5",
+        "primary-light": "#f2d4f2",
+        "primary-dark": "#b86bb8",
+        light: "#f2f2f2",
+        egray: "#2d2d2d",
+
       }
     }
   },
