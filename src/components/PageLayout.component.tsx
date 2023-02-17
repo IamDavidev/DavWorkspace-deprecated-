469a7b38c15c6f1b3836faae94e2a1bd57d96fa0
IamDavidev <davidlez2mgk@gmail.com>
2023-02-17 10:59:28 -0600

    [ styles 💄 ] : main page layout

--- src/components/PageLayout.component.tsx
+++ src/components/PageLayout.component.tsx
@@ -1,10 +1,14 @@
 import Head from 'next/head'
 import type { FC, ReactNode } from 'react'
 
+import { Inter } from '@next/font/google'
+
 export interface IPropsPageLayout {
   title: string
   children: ReactNode
 }
+
+const inter = Inter({ subsets: ['latin'] })
 
 const PageLayout: FC<IPropsPageLayout> = ({ title, children }): JSX.Element => {
   return (
@@ -13,7 +17,13 @@ const PageLayout: FC<IPropsPageLayout> = ({ title, children }): JSX.Element => {
         <title>{title}</title>
       </Head>
       <body>
-        <main>{children}</main>
+        <main
+          className={
+            'flex flex-col items-center text-white bg-secondary  min-h-screen ' +
+            inter.className
+          }>
+          {children}
+        </main>
       </body>
     </>
   )
