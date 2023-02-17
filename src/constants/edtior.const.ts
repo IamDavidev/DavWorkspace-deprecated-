f91c06287919d7553a74cb037c13f5a82ba9a646
IamDavidev <davidlez2mgk@gmail.com>
2023-02-17 10:58:43 -0600

    [  add ➕ ] : confgurations available of code.

---
+++ src/constants/edtior.const.ts
@@ -0,0 +1,13 @@
+export const EDITOR = {
+  THEMES: {
+    LIGHT: 'light',
+    DARK: 'dark'
+  },
+  LANGUAGES: {
+    JS: 'javascript',
+    TS: 'typescript',
+    HTML: 'html',
+    CSS: 'css',
+    MD: 'markdown'
+  }
+}
