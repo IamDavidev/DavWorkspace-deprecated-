9d82e57e1a1ac050ae056b09fd82b1c8d0829a69
itsDavidev <davidlez2mgk@gmail.com>
2023-02-23 23:31:52 -0600

    [ configurations  🌐 ] : global optons edior

--- src/components/Editor.component.tsx
+++ src/components/Editor.component.tsx
@@ -1,16 +1,48 @@
 import { type FC } from 'react'
 
 import Editor, { type EditorProps } from '@monaco-editor/react'
+import { JetBrains_Mono } from '@next/font/google'
+
+const jetBrainsMono = JetBrains_Mono({
+  subsets: ['latin']
+})
+
+export const globalEditorOptions = {
+  minimap: {
+    enabled: false
+  },
+  fontSize: 13,
+  fontWeight: '400',
+  lineHeight: 20,
+  scrollBeyondLastLine: false,
+  wordWrap: 'on',
+  wordWrapColumn: 80,
+  wrappingIndent: 'same',
+  automaticLayout: true,
+  tabSize: 2,
+  insertSpaces: true,
+  cursorStyle: 'line',
+  cursorWidth: 2,
+  cursorBlinking: 'smooth',
+  lineNumbers: 'off',
+  lineNumbersMinChars: 3
+}
 
 type PropsEditorLayout = EditorProps
 
 const EditorLayout: FC<PropsEditorLayout> = ({
   height,
+  className,
   ...props
 }): JSX.Element => {
   return (
     <div className='w-1/2 h-full sticky top-10   flex flex-col gap-1rem'>
-      <Editor height={height} {...props} />
+      <Editor
+        options={globalEditorOptions}
+        height={height}
+        {...props}
+        className={`${className ?? ' '}  ${jetBrainsMono.className}`}
+      />
     </div>
   )
 }
