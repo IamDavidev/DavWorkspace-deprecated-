c89c709686e2e6d33ca5fea07b77ad4a9b0148aa
itsDavidev <davidlez2mgk@gmail.com>
2023-02-23 23:29:11 -0600

    [ styles 💄 ] :  codes in previews mode

--- src/app/editor/components/PreviewLayout.tsx
+++ src/app/editor/components/PreviewLayout.tsx
@@ -1,15 +1,40 @@
 import { type FC } from 'react'
 import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+
 import '@/styles/MarkdownStyles.css'
 
 interface IPropsPreviewMode {
   value: string
 }
-
 export const PreviewMode: FC<IPropsPreviewMode> = ({ value }): JSX.Element => {
   return (
     <div className='w-1/2 p-2'>
-      <ReactMarkdown className='preview-md markdown-body'>
+      <ReactMarkdown
+        components={{
+          code: ({ node, inline, className, children, ...props }) => {
+            // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+            const match = /language-(\w+)/.exec(className ?? '')
+            // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+            return !inline && match != null ? (
+              <SyntaxHighlighter
+                style={oneDark}
+                language={match[1]}
+                PreTag='div'>
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            )
+          }
+        }}
+        className='preview-md markdown-body'
+        remarkPlugins={[remarkGfm]}>
         {value}
       </ReactMarkdown>
     </div>
