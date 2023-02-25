9341026d91c9d0f7a45ef0e391849083a9ea243f
IamDavidev <davidlez2mgk@gmail.com>
2023-02-24 21:04:50 -0600

    [ style 💄} : new colors theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,15 @@
 module.exports = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx}'
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        secondary: "#1b1b1b",
-        primary: "#db9ee5",
-        "primary-light": "#f2d4f2",
-        "primary-dark": "#b86bb8",
-        light: "#f2f2f2",
-        egray: "#2d2d2d",
-
+        bg: '#060606',
+        secondary: '#1b1b1b',
+        primary: '#db9ee5',
+        'primary-light': '#f2d4f2',
+        'primary-dark': '#b86bb8',
+        light: '#f2f2f2',
+        egray: '#2d2d2d'
       }
     }
   },
