7984a14481af8d296491766a46b919c5f69b6575
itsDavidev <davidlez2mgk@gmail.com>
2023-02-26 12:48:08 -0600

    [ performance ⚗️ ] : suspense in editor view

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -6,23 +6,34 @@ import { EDITOR } from '@/constants/edtior.const'
 import { PreviewMode } from './components/PreviewLayout'
 import EditorLayout from '@/components/Editor.component'
 import { useEditor } from '@/lib/hooks/useEditor.hook'
+import { Suspense } from 'react'
 
 const EditorPage: NextPage = (): JSX.Element => {
   const { onChangeHandler, onMountHandler, preview } = useEditor()
+  console.log(preview)
 
   return (
     <>
       <div className='w-full h-full flex flex-row py-8 rounded-2xl gap-4 overflow-hidden'>
-        <EditorLayout
-          onMount={onMountHandler}
-          width={'100%'}
-          defaultLanguage={EDITOR.LANGUAGES.MD}
-          defaultValue={'# File Markdown'}
-          height='calc(100vh - 4rem)'
-          className={'h-editor-layout'}
-          onChange={onChangeHandler}
-        />
-        <PreviewMode value={preview !== undefined ? preview : ''} />
+        <Suspense
+          fallback={
+            <div className='w-full h-full flex justify-center items-center'>
+              <div>Loading Suspense ...</div>
+            </div>
+          }>
+          <EditorLayout
+            onMount={onMountHandler}
+            width={'100%'}
+            defaultLanguage={EDITOR.LANGUAGES.MD}
+            defaultValue={'# File Markdown'}
+            height='calc(100vh - 4rem)'
+            className={'h-editor-layout'}
+            onChange={onChangeHandler}
+          />
+          {preview !== ' ' && (
+            <PreviewMode value={preview !== undefined ? preview : ''} />
+          )}
+        </Suspense>
       </div>
     </>
   )
