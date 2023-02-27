7fde245d05e4d1d0e6404fffc71d7ed264d7f3cf
IamDavidev <davidlez2mgk@gmail.com>
2023-02-27 16:35:26 -0600

    [  add ➕ ] :  new icons

---
+++ src/components/icons/LogIn.icon.tsx
@@ -0,0 +1,30 @@
+import { type IPropsIcon } from '@lib/models/Icon.interface'
+import { type FC } from 'react'
+
+export const LogInIcon: FC<IPropsIcon> = ({
+  color,
+  className,
+  styles
+}): JSX.Element => {
+  return (
+    <>
+      <svg
+        className={className}
+        width='24'
+        height='24'
+        viewBox='0 0 24 24'
+        fill='none'
+        style={styles}
+        xmlns='http://www.w3.org/2000/svg'>
+        <path
+          color={color}
+          d='M15 3H19C19.5304 3 20.0391 3.21071 20.4142 3.58579C20.7893 3.96086 21 4.46957 21 5V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H15M10 17L15 12M15 12L10 7M15 12H3'
+          stroke='black'
+          strokeWidth='2'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        />
+      </svg>
+    </>
+  )
+}
---
+++ src/lib/models/Icon.interface.ts
@@ -0,0 +1,7 @@
+export interface IPropsIcon {
+  color: string
+  width?: number
+  height?: number
+  className: string
+  styles?: React.CSSProperties
+}
