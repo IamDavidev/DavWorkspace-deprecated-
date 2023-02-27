1530ca3e0c5986e26be56eefc77e5a963eff8bde
itsDavidev <davidlez2mgk@gmail.com>
2023-02-26 18:12:21 -0600

    [ style 💄 ] : new ui slide nav

--- src/components/SlideNav.component.tsx
+++ src/components/SlideNav.component.tsx
@@ -5,10 +5,13 @@ const btnStyle = 'text-white bg-primary  rounded-2xl p-2 w-full'
 
 const SlideNav: FC = (): JSX.Element => {
   return (
-    <nav className='h-full max-w-[120px] bg-secondary p-2 rounded-2xl flex flex-col justify-between items-start sticky top-8'>
+    <nav className='h-full w-full text-link bg-secondary p-2 rounded-tr-2xl rounded-br-2xl flex flex-col justify-between items-start sticky top-8'>
       <div className='flex flex-col gap-4 my-4 w-full'>
-        <Link href={'/editor'}>Editor</Link>
-        <Link href={'/'}>Home</Link>
+        <button className='text-start tracking-wide'>New note</button>
+        <Link href={'/notebooks'}>
+          <span className='w-full tracking-wide'>Notebooks</span>
+        </Link>
+        <Link href={'/notebooks/all-notes'}>All Notes</Link>
       </div>
       <div className='flex flex-col gap-4 w-full'>
         <Link href={'/login'} className={btnStyle}>
