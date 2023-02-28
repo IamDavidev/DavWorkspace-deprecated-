f0b3f631cb7581bdd2aba1fa5567300bb3b3cb70
IamDavidev <davidlez2mgk@gmail.com>
2023-02-27 22:13:33 -0600

    [  `  add 💄 `  ]  - ui nav  - fix props icons  - new icons  - theme colors in enum

--- src/components/SlideNav.component.tsx
+++ src/components/SlideNav.component.tsx
@@ -1,25 +1,105 @@
+import { COLORS } from '@constants/colors.const'
 import Link from 'next/link'
 import { type FC } from 'react'
+import { BookIcon } from './icons/Book.icon'
+import { EditIcon } from './icons/Edit.icon'
+import { LogInIcon } from './icons/LogIn.icon'
+import { NewIcon } from './icons/new.icon'
+import { NotesIcon } from './icons/Notes.icon'
+import { UserPlus } from './icons/UserPlus.icon'
 
-const btnStyle = 'text-white bg-primary  rounded-2xl p-2 w-full'
+export const btnStyle = 'text-white bg-primary  rounded-2xl p-2 w-full'
+
+export interface IPropsLinkNav {
+  href?: string
+  label?: string
+  icon?: JSX.Element | React.ReactNode
+  classNameLink?: string
+  children?: React.ReactNode
+}
+
+export const LinkNav: FC<IPropsLinkNav> = ({
+  href,
+  label,
+  icon,
+  classNameLink,
+  children,
+  ...props
+}): JSX.Element => {
+  if (href === undefined || href === null)
+    return (
+      <button
+        className={`w-full h-full flex flex-row gap-3 px-2 items-center ${
+          classNameLink ?? ''
+        } `}
+        {...props}>
+        {Boolean(icon) && <span>{icon}</span>}
+        {label !== undefined ? (
+          <span className=' tracking-wide'>{label}</span>
+        ) : (
+          children
+        )}
+      </button>
+    )
+
+  return (
+    <>
+      <Link
+        href={href}
+        className={`w-full h-full flex flex-row gap-3 justify-start items-center px-2 ${
+          classNameLink ?? ''
+        } `}>
+        {Boolean(icon) && <span>{icon}</span>}
+        {label !== undefined ? (
+          <span className=' tracking-wide'>{label}</span>
+        ) : (
+          children
+        )}
+      </Link>
+    </>
+  )
+}
 
 const SlideNav: FC = (): JSX.Element => {
   return (
     <nav className='h-full w-full text-link bg-secondary p-2 rounded-tr-2xl rounded-br-2xl flex flex-col justify-between items-start sticky top-8'>
       <div className='flex flex-col gap-4 my-4 w-full'>
-        <button className='text-start tracking-wide'>New note</button>
-        <Link href={'/notebooks'}>
-          <span className='w-full tracking-wide'>Notebooks</span>
-        </Link>
-        <Link href={'/notebooks/all-notes'}>All Notes</Link>
+        <LinkNav
+          label='New note'
+          icon={<NewIcon className='w-5 h-5 ' color={COLORS.LINK} />}
+        />
+        <LinkNav
+          href='/notebooks'
+          label='Notebooks'
+          icon={<BookIcon className='w-5 h-5 ' color={COLORS.LINK} />}
+        />
+        <LinkNav
+          href='/notebooks/all-notes'
+          label='View notes'
+          icon={<NotesIcon className='w-5 h-5 ' color={COLORS.LINK} />}
+        />
+        <LinkNav
+          href='/dashboard/notes/5f9f1b9b0b9b9b0b9b0b9b0b'
+          label='Editor '
+          icon={<EditIcon className='w-5 h-5 ' color={COLORS.LINK} />}
+        />
       </div>
-      <div className='flex flex-col gap-4 w-full'>
-        <Link href={'/login'} className={btnStyle}>
-          <span className='mx-auto w-full'>Login</span>
-        </Link>
-        <Link href={'/register'} className={btnStyle}>
-          Register
-        </Link>
+      <div className='flex flex-col gap-4 w-full my-4'>
+        <LinkNav
+          href='/login'
+          // label='Login'
+          classNameLink={
+            'border border-primary bg-transparent p-2 rounded-lg mx-auto justify-center'
+          }
+          icon={<LogInIcon className='w-5 h-5' color={COLORS.PRIMARY} />}>
+          <span className='mx-auto w-full text-primary font-bold'>Login</span>
+        </LinkNav>
+        <LinkNav
+          href='/register'
+          icon={<UserPlus className='w-5 h-5' color={COLORS.BG} />}
+          classNameLink='border border-primary bg-primary p-2 rounded-lg mx-auto justify-center'>
+          <span className='mx-auto w-full text-bg font-bold'>Register</span>
+        </LinkNav>
       </div>
     </nav>
   )
--- src/components/icons/UserPlus.icon.tsx
+++ src/components/icons/UserPlus.icon.tsx
@@ -1,7 +1,7 @@
 import { type IPropsIcon } from '@lib/models/Icon.interface'
 import { type FC } from 'react'
 
-export const LogInIcon: FC<IPropsIcon> = ({
+export const UserPlus: FC<IPropsIcon> = ({
   color,
   className,
   styles
@@ -10,14 +10,12 @@ export const LogInIcon: FC<IPropsIcon> = ({
     <>
       <svg
         className={className}
-        width='24'
-        height='24'
         viewBox='0 0 24 24'
         fill='none'
         style={styles}
         xmlns='http://www.w3.org/2000/svg'>
         <path
-          d='M15 3H19C19.5304 3 20.0391 3.21071 20.4142 3.58579C20.7893 3.96086 21 4.46957 21 5V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H15M10 17L15 12M15 12L10 7M15 12H3'
+          d='M16 21V19C16 17.9391 15.5786 16.9217 14.8284 16.1716C14.0783 15.4214 13.0609 15 12 15H5C3.93913 15 2.92172 15.4214 2.17157 16.1716C1.42143 16.9217 1 17.9391 1 19V21M23 11H20M20 11H17M20 11V8M20 11V14M12.5 7C12.5 9.20914 10.7091 11 8.5 11C6.29086 11 4.5 9.20914 4.5 7C4.5 4.79086 6.29086 3 8.5 3C10.7091 3 12.5 4.79086 12.5 7Z'
           stroke={color}
           strokeWidth='2'
           strokeLinecap='round'
