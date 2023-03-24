fc49d110e37bfa67602f24384657f45664de3a3e
IamDavidev <davidlez2mgk@gmail.com>
2023-03-24 16:23:52 -0600

    [ ` Remove ogs · 🔇 ` ]  - remove logs in slide nav

--- src/components/SlideNav.component.tsx
+++ src/components/SlideNav.component.tsx
@@ -13,7 +13,6 @@ export const btnStyle = 'text-white bg-primary  rounded-2xl p-2 w-full'
 
 const SlideNav = async (): Promise<JSX.Element> => {
   const { user } = await UserServerRepository.getUserServer()
-  console.log(user)
   return (
     <nav className='h-full w-full text-link bg-secondary p-2 rounded-tr-2xl rounded-br-2xl flex flex-col justify-between items-start sticky top-8'>
       <div className='flex flex-col gap-6 my-4 w-full'>
@@ -37,7 +36,7 @@ const SlideNav = async (): Promise<JSX.Element> => {
           icon={<EditIcon className='w-5 h-5 ' color={COLORS.LINK} />}
         />
       </div>
-      {user != null ? <SignOutBtn /> : <SignInSocial />}
+      {user !== null ? <SignOutBtn /> : <SignInSocial />}
     </nav>
   )
 }
