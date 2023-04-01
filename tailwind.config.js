9e8005bdadb5e9a249f5493e9ec28c4b0b1ec188
IamDavidev <davidlez2mgk@gmail.com>
2023-04-01 13:07:31 -0600

    [ ` Update UI / styles / design · 💄 ` ] - new theme for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,18 @@ module.exports = {
       colors: {
         bg: '#060606',
         secondary: '#0d0d0d',
-        link: "#a7a9ac",
+        link: '#a7a9ac',
         primary: '#db9ee5',
         'primary-light': '#f2d4f2',
         'primary-dark': '#b86bb8',
         light: '#f2f2f2',
-        egray: '#2d2d2d'
+        egray: '#2d2d2d',
+        tray: '#393E46',
+        tlack: '#060606',
+        Tyan: '#62CDFF',
+        tose: '#DB9EE5',
+        talid: '#DBE2EF',
+        thite: '#FFFFFF'
       }
     }
   },
