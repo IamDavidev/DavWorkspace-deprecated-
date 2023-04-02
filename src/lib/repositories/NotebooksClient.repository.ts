cdb4e56a04ebc2a985267a283da32550876994a5
IamDavidev <davidlez2mgk@gmail.com>
2023-04-02 13:46:54 -0600

    [ ` Refactor code  · ♻️ ` ] - types - name methods

--- src/lib/repositories/NotebooksClient.repository.ts
+++ src/lib/repositories/NotebooksClient.repository.ts
@@ -1,29 +1,18 @@
 import { browserClient } from "@lib/clients/supbaseBrowser.client";
-import { type PostgrestError } from "@supabase/supabase-js";
 
-interface INoteBook {
-  id: string;
-  title: string;
-  description: string;
-  created_at: Date;
-  image: string;
-  owner_id: string;
-}
-interface ICreateNoteBookResponse {
-  error: PostgrestError | null;
-}
+import {
+  type INotebook,
+  type INotebookBadResponse,
+} from "@lib/models/Notebook.interface";
 
 export class NotebookBrowserRepository {
-  private async getClient(): Promise<string> {
-    return "";
-  }
-
-  public static async createNewNotebook(
-    notebook: INoteBook,
-  ): Promise<ICreateNoteBookResponse> {
+  public async createNew(
+    notebook: INotebook,
+  ): Promise<INotebookBadResponse> {
     const { error } = await browserClient.from("notebooks").insert([
       notebook,
     ]);
+
     return {
       error,
     };
--- src/lib/repositories/NotebooksServer.repository.ts
+++ src/lib/repositories/NotebooksServer.repository.ts
@@ -1,36 +1,12 @@
 import { serverClient } from "@lib/clients/supbaseServer.client";
-import { type PostgrestError } from "@supabase/supabase-js";
 
-type NotebooksType = Array<Record<string, any>> | [];
-
-interface INotebooksResponse {
-  notebooks: NotebooksType;
-  error: PostgrestError | null;
-  status: number | null;
-}
-
-export type OwnerIdType = string;
+import {
+  type INotebooksResponse,
+  type OwnerIdType,
+} from "@lib/models/Notebook.interface";
 
 export class NotebooksServerRepository {
-  private async getClient(): Promise<string> {
-    return "";
-  }
-
-  public static async getAllNotebooks(): Promise<INotebooksResponse> {
-    const {
-      data: notebooks,
-      error,
-      status,
-    } = await (await serverClient).from("notebooks").select("*");
-
-    return {
-      notebooks: notebooks ?? [],
-      error,
-      status,
-    };
-  }
-
-  public static async getNotebooksByOwnerId(
+  public async findByOwnerId(
     ownerId: OwnerIdType,
   ): Promise<INotebooksResponse> {
     const {
