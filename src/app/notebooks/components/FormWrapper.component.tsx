4182aac60ec22a328b789f7cd79c7b4719d878d7
IamDavidev <davidlez2mgk@gmail.com>
2023-04-05 16:29:57 -0600

    [ ` Refactor code  · ♻️ ` ] - refactor to create notebook

--- src/app/notebooks/components/FormWrapper.component.tsx
+++ src/app/notebooks/components/FormWrapper.component.tsx
@@ -1,10 +1,10 @@
 'use client'
 
+import { type FC, type FormEvent, type ReactNode } from 'react'
+import { toast } from 'sonner'
+
 import { apiCreateNotebook } from '@lib/api/createNoteebook.api'
 import { stylesToaster } from '@lib/constants/toasterStyles.const'
-import { type FormEvent, type FC, type ReactNode } from 'react'
-
-import { toast } from 'sonner'
 
 export interface FormWrapperProps {
   children: ReactNode
@@ -21,52 +21,61 @@ export function inputEmptyExceptionClient(nameInput: string): void {
   })
 }
 
+export const checkInputEmpty = (value: string): boolean => value.length < 1
+
+interface IHandlerProps {
+  evt: FormEvent
+}
+
+interface IHandlerOnSubmit extends IHandlerProps {
+  userId: string
+}
+
+export async function hanlderOnSubmit({
+  evt,
+  userId
+}: IHandlerOnSubmit): Promise<void> {
+  evt.preventDefault()
+  const form = evt.target as HTMLFormElement
+
+  const { title_notebook: title, description } = form
+
+  const titleInput = title.value
+  const descriptionInput = description.value
+
+  checkInputEmpty(titleInput) && inputEmptyExceptionClient('Title')
+  checkInputEmpty(description.value) && inputEmptyExceptionClient('Description')
+
+  const {
+    isOk,
+    error: { message }
+  } = await apiCreateNotebook({
+    title: titleInput,
+    description: descriptionInput,
+    image: null,
+    ownerId: userId
+  })
+
+  !isOk &&
+    toast.error(message, {
+      style: {
+        ...stylesToaster.error
+      }
+    })
+}
+
 export const FormCreateNotebookWrapper: FC<FormWrapperProps> = ({
   children,
   userId
 }): JSX.Element => {
-  const onSubmitHandler = async (e: FormEvent): Promise<void> => {
-    e.preventDefault()
-    const form = e.target as HTMLFormElement
-
-    const { title_notebook: title, description } = form
-
-    const titleValue = title.value
-    if (titleValue.length < 1) {
-      inputEmptyExceptionClient('Title')
-      return
-    }
-    const descriptionValue = description.value as string
-    if (descriptionValue.length < 1) {
-      inputEmptyExceptionClient('Description')
-    }
-
-    const {
-      isOk,
-      error: { message }
-    } = await apiCreateNotebook({
-      title: titleValue,
-      description: descriptionValue,
-      image: null,
-      ownerId: userId
-    })
-
-    if (!isOk) {
-      toast.error(message, {
-        style: {
-          ...stylesToaster.error
-        }
-      })
-    }
-
-    // router.push('/notebooks')
-  }
-
   return (
     <form
       className='flex flex-col justify-start items-center gap-8'
       onSubmit={e => {
-        onSubmitHandler(e).catch(console.error)
+        hanlderOnSubmit({
+          evt: e,
+          userId
+        }).catch(() => {})
       }}>
       {children}
     </form>
