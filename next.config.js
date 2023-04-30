8314e74256fa3153bf3a59c1248ecabbc748df5f
IamDavidev <davidlez2mgk@gmail.com>
2023-04-30 10:15:12 -0600

    [ ` update configuration files · 🔧 ` ] add domain image github in next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
+// (https://avatars.githubusercontent.com/u/85713808?v=4)
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images:{
+    domains: ["avatars.githubusercontent.com"]    
+  }
 }
 
 module.exports = nextConfig
