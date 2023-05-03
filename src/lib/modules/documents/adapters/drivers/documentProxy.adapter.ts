cb819c83e232c62cc278f2a9f7dd5f5c0b9b9bdd
IamDavidev <davidlez2mgk@gmail.com>
2023-05-03 17:38:12 -0600

    [ ` Introduced new features. · ✨ ` ] document proxy adapter

---
+++ src/lib/modules/documents/adapters/drivers/documentProxy.adapter.ts
@@ -0,0 +1,38 @@
+import {
+  type DocumentCreatingResult,
+  type DocumentEntity,
+  type DocumentToUpdating,
+  type ReponseDeletingDocument,
+  type ResponseOperation
+} from '../../main/entities/documet.entity'
+import {
+  type DocumentRepository,
+  type ForDocumentRepository
+} from '../../main/root'
+
+export class DocumentProxyAdapter implements ForDocumentRepository {
+  constructor(private readonly respository: DocumentRepository) {}
+  async getAllDocumentsByOwnerId(
+    ownerId: string
+  ): Promise<DocumentEntity[] | null> {
+    return await this.respository.getAllDocumentsByOwnerId(ownerId)
+  }
+
+  async createDocument(document: DocumentEntity): Promise<ResponseOperation> {
+    return await this.respository.createDocument(document)
+  }
+
+  async updateDocument(
+    document: DocumentToUpdating
+  ): Promise<DocumentCreatingResult> {
+    return await this.respository.updateDocument(document)
+  }
+
+  async deleteDocument(id: string): Promise<ReponseDeletingDocument> {
+    return await this.respository.deleteDocument(id)
+  }
+
+  async getDocumentById(id: string): Promise<DocumentEntity | null> {
+    return await this.respository.getDocumentById(id)
+  }
+}
