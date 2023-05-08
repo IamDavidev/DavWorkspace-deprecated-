5f30989ae221e93fb8443ab43075e9df19b4d7f0
IamDavidev <davidlez2mgk@gmail.com>
2023-05-07 19:30:14 -0600

    [ ` Update files / components · 📦 ` ] add for tags ui

--- src/app/dashboard/documents/components/DocumentItem.component.tsx
+++ src/app/dashboard/documents/components/DocumentItem.component.tsx
@@ -1,23 +1,43 @@
-import { type DocumentEntity } from '@lib/modules/documents/main/entities/documet.entity';
 import { type FC } from 'react'
+
+import { type DocumentEntity } from '@lib/modules/documents/main/entities/documet.entity';
+
+export const TagItem: FC<{
+  name: string
+}> = ({ name }): JSX.Element => {
+  return (
+    <div>
+      <span>
+        {name}
+      </span>
+    </div>
+  )
+}
 
 export const DocumentItem: FC<DocumentEntity> = ({
   title,
   status,
-  updated_at
+  updated_at: updatedAt,
+  tags
 }): JSX.Element => {
-  console.info("🚀 ~>  file: DocumentItem.component.tsx:9 ~>  updated_at:", updated_at)
   return (
     <article className='rounded-lg p-2 border border-white border-solid'>
       <header>
         <div>
           <span>{status}</span>
           <span>
-            {new Date(updated_at)}
+            {String(new Date(updatedAt))}
           </span>
         </div>
         <h2 className='text-2xl font-bold '>{title}</h2>
       </header>
+      <div>
+        {tags.map((tag): JSX.Element => {
+          return (
+            <TagItem key={tag} name={tag} />
+          )
+        })}
+      </div>
     </article>
   )
 };
