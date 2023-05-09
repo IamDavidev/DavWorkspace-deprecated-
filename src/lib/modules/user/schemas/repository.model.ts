74da34e1f76763fd6e2d67c713e0cf3512129998
IamDavidev <davidlez2mgk@gmail.com>
2023-05-08 20:04:53 -0600

    [ ` Fix / simple non-critical issue. · 🩹 ` ] fix export type

--- src/lib/modules/user/schemas/repository.model.ts
+++ src/lib/modules/user/schemas/repository.model.ts
@@ -1,15 +1,12 @@
-import { type IUser } from "./user.model";
+import { type PersistanceUser } from './user.model'
 
 export interface IUserAuthRepository {
-  signInWithGithub: () => Promise<void>;
-  signInWithGoogle: () => Promise<void>;
-  signInWithEmailAndPassword: (
-    email: string,
-    password: string,
-  ) => Promise<void>;
-  signOut: () => Promise<void>;
+  signInWithGithub: () => Promise<void>
+  signInWithGoogle: () => Promise<void>
+  signInWithEmailAndPassword: (email: string, password: string) => Promise<void>
+  signOut: () => Promise<void>
 }
 
 export interface IUserRepository {
-  getCurrentUser: () => Promise<IUser | null>;
+  getCurrentUser: () => Promise<PersistanceUser | null>
 }
