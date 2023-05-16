2a65c93ced9a3b9d8de109e90a6aef40a343a710
IamDavidev <davidlez2mgk@gmail.com>
2023-05-16 08:45:23 -0600

    [ ` Fix / critical hotfix · 🚑 ` ] rederict error bucle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,8 @@ import { type ReactNode } from 'react'
 import '../styles/editor.styles.css'
 import '../styles/global.css'
 
-import SlideNav from '@/components/SlideNav.component'
 import { ContainerToaster } from '@components/common/ContainerToaster.component'
-import { compositionRootUser } from '@lib/modules/user/compositionRootUser'
 import UserProvider from '@lib/modules/user/UserProviderComponent'
-import { redirect, } from 'next/navigation'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,14 +15,6 @@ export default async function RootLayout({
 }: {
   children: ReactNode
 }): Promise<JSX.Element> {
-
-  const { userRepository } = compositionRootUser()
-  const user = await userRepository.getCurrentUser()
-
-  if (user === null) {
-    redirect('/')
-  }
-
   return (
     <html lang='en'>
       <body>
@@ -35,13 +24,6 @@ export default async function RootLayout({
               ' app-editor  flex  flex-row gap-0 min-h-screen text-white bg-dark ' +
               inter.className
             }>
-            {
-              user !== null && (
-                <section className='max-w-[250px] w-[250px]'>
-                  <SlideNav imageSrc={user.avatar_url ?? ""} name={user.nickname} email={user.email ?? ""} />
-                </section>
-              )
-            }
             <main className='w-full h-auto mx-auto'>
               {children}
             </main>
@@ -49,6 +31,6 @@ export default async function RootLayout({
           <ContainerToaster />
         </UserProvider>
       </body>
-    </html>
+    </html >
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,16 @@ export const metadata = {
 
 const Home = (): JSX.Element => {
   return (
-    <section className='p-4'>
-      <Link href={'/auth/login'} >
+    <section className='p-4 flex flex-row gap-6 h-full w-full justify-center items-center '>
+      <Link href={'/auth/login/'}
+        className="bg-white text-dark-gray rounded-lg py-2 px-6 hover:bg-dark-gray hover:text-white transition-all hover:duration-500"
+      >
         Login
+      </Link>
+      <Link href={'/dashboard/'}
+        className="bg-white text-dark-gray rounded-lg py-2 px-6 hover:bg-dark-gray hover:text-white transition-all hover:duration-500"
+      >
+        Dashboard
       </Link>
     </section>
   )
