6435ed7cf6804dc5f017777aae08db4f237fef25
IamDavidev <davidlez2mgk@gmail.com>
2023-06-11 09:27:17 -0600

    [ ` Fix / simple non-critical issue. · 🩹 ` ] types in repository

--- src/lib/modules/documents/main/root.ts
+++ src/lib/modules/documents/main/root.ts
@@ -1,24 +1,27 @@
 import { type RepositoryQuerier } from '../adapters/drivens/repository-querier.adapter'
 import { type ForControlOperating } from '../ports/drivens/ControlOperating.port'
 import { type RepositoryQuerying } from '../ports/drivens/RepositoryQuerying.port'
-import {
-  type DocumentCreatingResult,
-  type DocumentEntity,
-  type DocumentToUpdating,
-  type ResponseDeletingDocument,
-  type ResponseOperation
+import type {
+  DocumentEntity,
+  DocumentToUpdating,
+  ResponseDeletingDocument,
+  ResponseOperation,
+  ResponseUpdatingDocument
 } from './entities/documet.entity'
+
 import { UUID } from '@lib/modules/shared/uuid.type'
 
 export interface ForDocumentRepository
   extends RepositoryQuerying,
-    ForControlOperating {}
+    ForControlOperating {
+}
 
 export class DocumentRepository implements ForDocumentRepository {
   constructor(
     private readonly controlOperating: ForControlOperating,
     private readonly repositoryQuerier: RepositoryQuerier
-  ) {}
+  ) {
+  }
 
   async getDocumentById(id: string): Promise<DocumentEntity | null> {
     return await this.repositoryQuerier.getDocumentById(id)
@@ -37,7 +40,7 @@ export class DocumentRepository implements ForDocumentRepository {
   async updateDocument(
     document: DocumentToUpdating,
     documentId: UUID
-  ): Promise<DocumentCreatingResult> {
+  ): Promise<ResponseUpdatingDocument> {
     return await this.controlOperating.updateDocument(document, documentId)
   }
 
