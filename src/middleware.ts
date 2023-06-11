88714c138596c065b87ceb600e486b440ae4bf0c
IamDavidev <davidlez2mgk@gmail.com>
2023-06-11 16:24:43 -0600

    [ ` Refactor code  · ♻️ ` ] logger in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,8 @@ import { type NextRequest, NextResponse } from 'next/server'
 import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs'
 
 export async function middleware(req: NextRequest): Promise<NextResponse> {
+  console.log("Middleware called")
+  
   const res = NextResponse.next()
 
   const client = createMiddlewareSupabaseClient({
