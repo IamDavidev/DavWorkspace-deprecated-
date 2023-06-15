35d52aefea6b4c4bcddf58a661bc9203995eb4f8
IamDavidev <davidlez2mgk@gmail.com>
2023-06-15 12:53:10 -0600

    [ ` Introduced new features. · ✨ ` ] action create new document

--- src/components/BtnCreateNewDocument.tsx
+++ src/components/BtnCreateNewDocument.tsx
@@ -4,13 +4,37 @@ import type { FC } from 'react'
 import { COLORS } from '@constants/colors.const'
 import { NewIcon } from '@components/icons'
 
+import { useRouter } from 'next/navigation'
+import { type AppRouterInstance } from 'next/dist/shared/lib/app-router-context'
+
+
+export async function createNeDocumentAction(router: AppRouterInstance): Promise<void> {
+
+  await fetch('/api/create-new-document', {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'POST'
+  }).then((res: Response): any => res.json())
+    .then(res => {
+      const { status, id } = res
+      if (status !== 200) return
+      const url = `/dashboard/editor/document/${id as string}`
+      router.push(url)
+    })
+}
+
 
 export const BtnCreateNewDocument: FC = () => {
+  const router = useRouter()
+
   return (
     <>
       <button
-        onClick={() => {
-          console.log('click 🆕 ')
+        onClick={(): void => {
+          createNeDocumentAction(router).catch(err => {
+            console.log(err)
+          })
         }}
         className={`w-full h-full flex flex-row gap-3 justify-start items-center`}>
         <NewIcon className='w-6 h-6' color={COLORS.WHITE} />
