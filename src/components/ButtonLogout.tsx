00110917caac23df5578c6ce5146822b8c67d937
IamDavidev <davidlez2mgk@gmail.com>
2023-06-15 16:11:37 -0600

    [ ` Refactor code  · ♻️ ` ] action logout action

--- src/components/ButtonLogout.tsx
+++ src/components/ButtonLogout.tsx
@@ -1,20 +1,32 @@
 'use client'
+import { type  FC } from 'react'
+import { useRouter } from 'next/navigation'
+import { type  AppRouterInstance } from 'next/dist/shared/lib/app-router-context'
 
 import { COLORS } from '@constants/colors.const'
 import { compositionRootUserAuth } from '@lib/modules/user/main/compositionRootUserAuth'
 import { compositionRootLogger } from '@lib/modules/logger/root'
 import { LogOutIcon } from '@components/icons'
 
-export const ButtonLogout = (): JSX.Element => {
-  const { logger } = compositionRootLogger()
+
+export async function logoutAction(router: AppRouterInstance): Promise<void> {
   const { userAuthProxy } = compositionRootUserAuth()
+
+  await userAuthProxy.signOut()
+
+  router.push('/login')
+}
+
+export const ButtonLogout: FC = () => {
+  const router = useRouter()
+  const { logger } = compositionRootLogger()
 
   return (
     <button
       className='px-6 py-1 bg-white text-dark-gray  rounded-lg flex flex-row gap-2 opacity-80 hover:opacity-100 justify-center items-center'
       onClick={() => {
-        userAuthProxy.signOut().catch(() => {
-          logger.error('Error with sign out')
+        logoutAction(router).catch(() => {
+          logger.error('Logout error')
         })
       }}
     >
