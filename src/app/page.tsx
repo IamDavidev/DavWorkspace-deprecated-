3b1abf7d991a11c310d231f18009e1ead39989ba
IamDavidev <davidlez2mgk@gmail.com>
2023-06-17 09:36:05 -0600

    [ ` Remove code or files · 🗑️ ` ] remove comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,26 +20,3 @@ const Home = async (): Promise<JSX.Element> => {
 }
 
 export default Home
-
-/**
- *
- *
- *     <section className='p-4 flex flex-row gap-6 h-full w-full justify-center items-center '>
- *       <Link href={'/auth/login/'}
- *             className='bg-white text-dark-gray rounded-lg py-2 px-6 hover:bg-dark-gray hover:text-white transition-all hover:duration-500'
- *       >
- *         Login
- *       </Link>
- *       <Link href={'/dashboard/'}
- *             className='bg-white text-dark-gray rounded-lg py-2 px-6 hover:bg-dark-gray hover:text-white transition-all hover:duration-500'
- *       >
- *         Dashboard
- *       </Link>
- *       <Link href={'/editor/md/'}
- *             className='bg-white text-dark-gray rounded-lg py-2 px-6 hover:bg-dark-gray hover:text-white transition-all hover:duration-500'
- *       >
- *         Markdown Editor
- *       </Link>
- *     </section>
- *
- */
