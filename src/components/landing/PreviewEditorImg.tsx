36ca5ba687c6867cdb0d75a5f10c1d79fe55c59d
IamDavidev <davidlez2mgk@gmail.com>
2023-06-17 09:39:45 -0600

    [ ` Fix / critical hotfix · 🚑 ` ] fix lint errror

--- src/components/landing/PreviewEditorImg.tsx
+++ src/components/landing/PreviewEditorImg.tsx
@@ -7,13 +7,7 @@ import { type  FC } from 'react'
 export const PreviewEditorImg: FC = () => {
   return (
     <div className='w-[80%] mx-auto rounded-2xl pb-16'>
-      <Atropos
-        activeOffset={60}
-        shadowScale={1.05}
-        onEnter={() => console.log('Enter')}
-        onLeave={() => console.log('Leave')}
-        onRotate={(x, y) => console.log('Rotate', x, y)}
-      >
+      <Atropos>
         <img src={'/previewEditor.png'} alt={'preview editor'}
              className={'w-full rounded-2xl border-light-violet border border-solid'} />
       </Atropos>
