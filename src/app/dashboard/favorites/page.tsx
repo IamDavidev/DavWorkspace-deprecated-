db326c1ec9edab6f5d0e27eb3d3f76685d582281
IamDavidev <davidlez2mgk@gmail.com>
2023-06-19 12:31:10 -0600

    [ ` Introduced new features. · ✨ ` ] add page favorites

--- src/app/dashboard/favorites/page.tsx
+++ src/app/dashboard/favorites/page.tsx
@@ -1,6 +1,8 @@
 import { compositionRootDocument } from '@lib/modules/documents/main/compositionRootDocuments'
 import { compositionRootUser } from '@lib/modules/user/main/compositionRootUser'
 import { redirect } from 'next/navigation'
+import { COLORS } from '@constants/colors.const'
+import { ExpandIcon } from '@components/icons/Expand.icon'
 
 
 const FavoritesPage = async (): Promise<JSX.Element> => {
@@ -18,16 +20,40 @@ const FavoritesPage = async (): Promise<JSX.Element> => {
     ? documents.filter(document => document.isFavorite)
     : []
 
-
-  console.log(favoritesDocuments)
-
   return (
     <div
-      className={'flex flex-col justify-center items-center w-full h-full '}
+      className={'flex flex-col w-full h-full gap-4 p-16'}
     >
       <h1
-        className={'text-4xl font-bold '}
+        className={'text-4xl font-bold w-full text-end'}
       >Favorites</h1>
+      <div className={'flex flex-row gap-4 justify-center items-stretch'}>
+        {favoritesDocuments.map((doc) => {
+          return (
+            <article key={doc.id} className={'p-4 border border-solid border-white rounded-2xl flex flex-col gap-2'}>
+              <header className={'w-full flex flex-row justify-between'}>
+                <span className={'text-white opacity-80 '}>Academy</span>
+                <a href={`/dashboard/documents/${doc.id}`}>
+                  <ExpandIcon className={'w-6 h-6 text-white opacity-80'} strokeColor={COLORS.WHITE} />
+                </a>
+              </header>
+              <div>
+                <h2 className={'text-2xl'}> {doc.title} </h2>
+              </div>
+              <footer className={'flex flex-row gap-2 pt-2'}>
+                {doc.tags.length > 0 ? doc.tags.map(tag => {
+                  return (
+                    <div key={tag} className={'border border-light-violet border-solid px-8 py-1 rounded-full'}>
+                      <span className={'text-light-violet'}>{tag}</span>
+                    </div>
+                  )
+                }) : <span
+                  className={'text-white border-white border-solid border rounded-full opacity-80 px-8 p-1'}>no tags</span>}
+              </footer>
+            </article>
+          )
+        })}
+      </div>
     </div>
   )
 }
