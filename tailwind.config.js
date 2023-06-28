1438ce418ca060c4100b8e96406cab39c653f014
IamDavidev <davidlez2mgk@gmail.com>
2023-06-28 12:44:42 -0600

    [ ` Update UI / styles / design · 💄 ` ] new color bg editor in config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "white" : "#DBE2EF",
-        "dark" : "#111111",
-        "dark-gray": "#191919",
-        "light-blue": "#62cdff",
-        "light-violet": "#8a8aff",
+        'white': '#DBE2EF',
+        'dark': '#111111',
+        'dark-gray': '#191919',
+        'light-blue': '#62cdff',
+        'light-violet': '#8a8aff',
+        'bg-editor': '#0d0d0d'
       }
     }
   },
